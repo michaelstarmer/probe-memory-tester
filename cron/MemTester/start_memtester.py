f9eacb505a8fd53dd348813faa0d429215b14701
Michael Starmer <michael.pedersen.starmer@bridgetech.tv>
2022-04-04 10:49:43 +0200

    python3 env

--- cron/MemTester/start_memtester.py
+++ cron/MemTester/start_memtester.py
@@ -11,7 +11,7 @@ import logging
 from sys import stdin, stdout, stderr
 import paramiko
 
-API_HOST = os.getenv('API_HOST', 'http://localhost:3333')
+API_HOST = os.environ.get('API_HOST', 'http://localhost:3333')
 
 
 def set_snapshot(sid):
