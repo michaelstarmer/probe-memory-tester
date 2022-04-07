7adeef622c557eb514d89b4603b7b610cb4c919c
= <=>
2022-04-07 15:03:20 +0200

    fix stress-ng

--- cron/MemTester/stress.py
+++ cron/MemTester/stress.py
@@ -29,7 +29,10 @@ def set_memory(RHOST, MEMORY, DURATION):
     client.connect(RHOST, username='root',
                    password='elvis', timeout=DURATION*60)
     transport = client.get_transport()
-    channel = transport.open_session()
+    channel1 = transport.open_session(timeout=200)
+    
+    channel2 = transport.open_session(timeout=200)
+    
     print('Install stress-ng')
     print('stress-ng install: OK')
     print('Executing stress-ng memory test...')
@@ -39,7 +42,9 @@ def set_memory(RHOST, MEMORY, DURATION):
                 --vm 1
                 --timeout {DURATION}M
                 > /dev/null 2>1 &""")
-
-    channel.exec_command(
-        f"""yum install -y stress-ng & killall stress-ng-vm ; stress-ng --vm-bytes {MEMORY}G --vm-keep --vm 1 --timeout {DURATION}M > /dev/null 2>&1 &""")
+    print('Run channel 1 (yum)')
+    channel1.exec_command(f"""yum install -y stress-ng""")
+    print('Run channel 2 (stress-ng)')
+    channel2.exec_command(
+        f'killall stress-ng-vm ; stress-ng --vm-bytes {MEMORY}G --vm-keep --vm 1 --timeout {DURATION}M > /dev/null 2>&1 &')
 
