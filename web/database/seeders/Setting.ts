f55e896c98e07c621e25571250e996769cef7f60
Michael Starmer <michael.pedersen.starmer@bridgetech.tv>
2022-05-18 13:05:59 +0200

    button style edit

--- web/database/seeders/Setting.ts
+++ web/database/seeders/Setting.ts
@@ -7,7 +7,7 @@ export default class SettingsSeeder extends BaseSeeder {
     // Write your database queries inside the run method
     try {
       await Setting.createMany([
-        { key: 'probe_ip', value: '10.0.28.239' },
+        { key: 'probe_ip', value: '10.0.28.140' },
         { key: 'vm_name', value: 'sw-probe-memtest' },
         { key: 'job_interval', value: '3000' },
         { key: 'jenkins_job', value: 'CentOS7-based_6.1' },
