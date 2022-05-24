5809714ab39fc10b1d0fd77c4cbc2025c26b7fe5
Michael Starmer <michael.pedersen.starmer@bridgetech.tv>
2022-05-24 11:08:46 +0200

    Organized routes

--- web/start/routes.ts
+++ web/start/routes.ts
@@ -1,6 +1,8 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
+Route.get('apidoc', async ({ view }) => view.render('api-doc'))
+
 Route.get('/', 'AppController.index').as('home')
 Route.get('/api/probe-config', 'AppController.get_probe_config')
 Route.get('/api/jobs', 'ApiController.all_jobs')
@@ -16,23 +18,20 @@ Route.get('/api/jobs/:id/start', 'ApiController.start_job_by_id')
 Route.post('/api/jobs/:id/log', 'ApiController.create_job_log')
 Route.get('/api/jobs/:id/log', 'ApiController.get_job_log')
 
-
-Route.post('/api/queue', 'ApiController.create_job')
-Route.get('/api/queue/next', 'ApiController.next_job')
-Route.get('/api/queue/active', 'ApiController.running_job')
-
 Route.post('/api/stats', 'SystemStatController.create')
 Route.get('/api/stats/job/:jobId', 'SystemStatController.stats_by_job_id')
 Route.post('/api/stats/job/:jobId', 'SystemStatController.create_by_job')
-
 Route.post('/api/stats/btech/:jobId', 'BtechProcController.create')
+
+/* Deprecated routes */
+Route.post('/api/queue', 'ApiController.create_job')
+Route.get('/api/queue/next', 'ApiController.next_job')
+Route.get('/api/queue/active', 'ApiController.running_job')
 
 Route.get('/api/xml', 'JobsController.get_all_xml')
 Route.get('/uploads', 'FilesController.upload_view')
 Route.post('/upload', 'FilesController.upload_file')
 Route.get('/sessionflash', 'FilesController.session_flash')
-
-Route.get('apidoc', async ({ view }) => view.render('api-doc'))
 
 Route.get('/host/edit', 'AppController.edit_host').as('edit.host')
 Route.get('/settings', 'AppController.edit_host')
