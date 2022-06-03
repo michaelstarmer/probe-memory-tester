fc7df635d43cd3dab91c1ffa651ad2380cd793bd
= <=>
2022-06-03 15:20:05 +0200

    details report

--- cron/MemTester/btech_gvm.py
+++ cron/MemTester/btech_gvm.py
@@ -79,7 +79,7 @@ class GvmSSH:
         return payload['get_reports_response']
 
     def downloadReportPDF(self, report_id, path='report.pdf'):
-        cmd = f'gvm-cli --gmp-username admin --gmp-password ldap2retro socket --xml "<get_reports report_id=\\"{report_id}\\" format_id=\\"c402cc3e-b531-11e1-9163-406186ea4fc5\\"/>"'
+        cmd = f'gvm-cli --gmp-username admin --gmp-password ldap2retro socket --xml "<get_reports report_id=\\"{report_id}\\" details=\\"1\\" format_id=\\"c402cc3e-b531-11e1-9163-406186ea4fc5\\"/>"'
         payload = self._sshExec(cmd)
         content = payload['get_reports_response']['report']["#text"]
         binaryBase64EncodedPdf = str(content).encode('ascii')
