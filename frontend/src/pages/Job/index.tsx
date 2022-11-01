f0f0cc8e3c452213bc6c71bd603f904c677119d7
Michael Starmer <michael.starmer@gmail.com>
2022-11-01 22:28:17 +0100

    Added recharts to react

--- frontend/src/pages/Job/index.tsx
+++ frontend/src/pages/Job/index.tsx
@@ -4,6 +4,8 @@ import { atom, useAtom } from 'jotai';
 import api from '../../utils/api'
 import { useParams, Link } from "react-router-dom";
 import "./Job.css"
+import MemoryChart from '../../features/chart/Chart'
+
 const jobData = atom(async (get) => {
     let { id } = useParams()
     const url = `http://localhost:3333/api/jobs/${id}`
@@ -21,56 +23,69 @@ const ETable = styled.table`
     color: rgb(209, 205, 199);
     border-color: rgb(56, 61, 63);
 `
-const TableLink = styled(Link)`
-    text-decoration: none;
-`
+
 const Form = styled.form`
     display: inline;
 `
 
-const Table = ({ jenkins_job, build_number, duration, started_at }) => {
+const TableLink = styled(Link)`
+    text-decoration: none;
+`
+
+const C_Link = (props, {to}) => {
+    return props.href ?
+    <a href={props.href} target="_blank">{props.children}</a> :
+    <TableLink to={props.to} target="_blank">{props.children}</TableLink>
+}
+const C_Link_External = ({to, text, ...props}) => {
+    return (
+        <a href={to} target="_blank">{text}</a>
+    )
+}
+const TableRow = (props) => {
+    return (
+        <tr>
+            <th>{props.title}</th>
+            <td>
+                {props.children}
+            </td>
+        </tr>
+    )
+}
+
+const Table = ({ jenkins_job, memory, dash_version, build_number, git_commit, duration, started_at }) => {
     return (
         <ETable className="table">
             <tbody>
-                <tr>
-                    <th>Jenkins job</th>
-                    <td><TableLink target="_blank"
-                        to="http://build.dev.btech/job/{{job.jenkinsJob}}">{jenkins_job}</TableLink>
-                    </td>
-                </tr>
-                <tr>
-                    <th>Git commit</th>
-                    <td>
-                        N/A
-                    </td>
-                </tr>
-                <tr>
-                    <th>Build no.</th>
-                    <td>
-                        <TableLink target="_blank"
-                            to={`http://build.dev.btech/job/${jenkins_job}/${build_number}`}>{build_number}</TableLink>
-                    </td>
-                </tr>
-                <tr>
-                    <th>Duration</th>
-                    <td>
-                        <span id="minutes-remaining">{duration} minutes</span>
-                    </td>
-                </tr>
-                <tr>
-                    <th>Memory stress</th>
-                    <td>Not applied</td>
-                </tr>
-                <tr>
-                    <th>Config</th>
-                    <td>
-                        <TableLink target="_blank" href="/public/uploads/xml/">XML</TableLink>
-                    </td>
-                </tr>
-                <tr>
-                    <th>Created at</th>
-                    <td>{started_at}</td>
-                </tr>
+                <TableRow title="Jenkins Job">
+                    <C_Link to={`http://build.dev.btech/job/${jenkins_job}`}>
+                        {jenkins_job} (#{build_number})
+                    </C_Link>
+                </TableRow>
+                <TableRow title="Git commit">
+                    <C_Link href={`http://git.dev.btech/cgi-bin/cgit.cgi/btech.git/commit/?id=${git_commit}`}>
+                        {git_commit}
+                    </C_Link>
+                </TableRow>
+                <TableRow title="Build">
+                    <C_Link target="_blank" href={`http://build.dev.btech/job/${jenkins_job}/${build_number}`}>
+                        {dash_version}
+                    </C_Link>
+                </TableRow>
+                <TableRow title="Duration">
+                    {duration} minutes
+                </TableRow>
+                <TableRow title="Memory stress">
+                    { memory ? memory : 'Not applied' }
+                </TableRow>
+                <TableRow title="Config">
+                    <C_Link to="/public/uploads/xml/">
+                        XML
+                    </C_Link>
+                </TableRow>
+                <TableRow title="Started at">
+                    { started_at ? started_at : 'N/A' }
+                </TableRow>
             </tbody>
         </ETable>
     )
@@ -95,11 +110,13 @@ const StatusHeader = ({ status }) => {
 
 const Job = () => {
     const [job] = useAtom(jobData);
+    console.log(job)
     return (
         <>
             <StatusHeader status={job.status} />
             <Hero className="container-fluid container-dark">
                 <div className="container py-5">
+
                     <div className="row">
                         <div className="col-md-12 col-lg-4 mb-3 d-flex flex-column justify-content-center">
                             <div className="row">
@@ -107,20 +124,23 @@ const Job = () => {
                                     <Table {...job} />
                                 </div>
                             </div>
+                            <div className="row my-3">
+                                <div className="col-12">
+                                    <button type="button" className="btn btn-secondary" data-bs-toggle="modal"
+                                        data-bs-target="#exampleModal">
+                                        Commands
+                                    </button>
+                                    <Form id="frm-stop-job" action="/jobs/{{ job.id }}/stop" method="get"
+                                        className="">
+                                        <button type="submit" id="btn-stop-job" className="btn btn-danger">
+                                            Stop test
+                                        </button>
+                                    </Form>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div className="row my-3">
-                        <div className="col-12">
-                            <button type="button" className="btn btn-secondary" data-bs-toggle="modal"
-                                data-bs-target="#exampleModal">
-                                Commands
-                            </button>
-                            <Form id="frm-stop-job" action="/jobs/{{ job.id }}/stop" method="get"
-                                className="">
-                                <button type="submit" id="btn-stop-job" className="btn btn-danger">
-                                    Stop test
-                                </button>
-                            </Form>
+                        <div id="chart" className="col-12 col-lg-6 offset-lg-2">
+                            <MemoryChart data={job.systemStats} />
                         </div>
                     </div>
                 </div>
