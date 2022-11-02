d41f7976d8ec405e4054893739b2c9617d4c1ab1
Michael Starmer <michael.pedersen.starmer@bridgetech.tv>
2022-11-02 05:54:49 +0100

    Add fetching of job logs

--- frontend/src/features/jobLog/index.jsx
+++ frontend/src/features/jobLog/index.jsx
@@ -1,9 +1,35 @@
 import React from "react";
+import { atom, useAtom } from 'jotai';
+import api from '../../utils/api'
+import { useParams } from "react-router-dom";
 
-export function JobLog() {
+const logData = atom(async (get) => {
+    let { id } = useParams()
+    const url = `http://localhost:3333/api/jobs/${id}/log`;
+    const response = await api.get(url)
+    if (response && response.data) {
+        console.log(response.data)
+        return response.data;
+    }
+})
+
+const renderLog = (logs) => {
+    if (!logs) {
+        return <div id="log-loading">Loading logs...</div>
+    }
+    return logs.map(element => {
+        return <div className=''>
+            {element.type}: {element.message}
+        </div>
+    });
+}
+
+export function JobLog({ id }) {
+    const [ log ] = useAtom(logData)
+    console.log(log)
     return (
         <div id="log" className="snippet">
-            <div id="log-loading">Loading logs...</div>
+            {renderLog(log)}
         </div>
 
     )
